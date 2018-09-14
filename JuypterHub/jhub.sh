6fb93edf78171e1965ddc989e4dd99305b5ce09a
Ricky Laney <rlaneyjr@gmail.com>
2018-09-13 21:38:41 -0400

    The git-update script pushed these

--- JuypterHub/jhub.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-RELEASE=jhub
-NAMESPACE=jhub
-
-helm upgrade --install $RELEASE jupyterhub/jupyterhub \
-  --namespace $NAMESPACE  \
-  --version 0.7.0-beta.2 \
-  --values config.yaml
